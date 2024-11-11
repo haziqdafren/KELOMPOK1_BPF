5dcf6442f9326ea427b2af726a0ba11c04762bd1
SitiSolikhah23TID <siti23ti@mahasiswa.pcr.ac.id>
2024-11-11 11:59:55 +0700

    siti ubah

--- projectWebwizard/resources/views/index.blade.php
+++ projectWebwizard/resources/views/index.blade.php
@@ -10,14 +10,6 @@
         <h1>Mohamad Haziq Dafren</h1>
         <h1>Siti Solikhah</h1>
         <h1>Nazwa Salsabila Halim</h1>
-<<<<<<< HEAD
         <h1>Luthfiah Rahmi</h1>
-=======
-        <h1>Lutfiah rahmi</h1>
-        <h1>Lutfiah rahmi</h1>
-        <h1>Siti Solikhah ke 2</h1>
-        <h1>Luthfiah Rahmi 2</h1>
-</body>
->>>>>>> 5ae09f08f2503e9ffd22ee2f172a578c56e64cba
 </body>
 </html>

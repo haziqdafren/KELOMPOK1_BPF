a5d18f5d4fde47e1390188f6775154954020b875
Nazwa62 <nazwa23ti@mahasiswa.pcr.ac.id>
2024-11-18 07:28:14 +0700

    Merge branch 'main' of https://github.com/haziqdafren/WebWizards

---
+++ projectWebwizard/resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nama Kelompok</title>
+</head>
+<body>
+        <h1>Mohamad Haziq Dafren</h1>
+        <h1>Mohamad Haziq Dafren</h1>
+        <h1>Nazwa Salsabila Halim</h1>
+        <h1>Muhammad Atha Ananda </h1>
+        <h1>Siti Solikhah </h1>
+        <h1>Luthfia Rahmi</h1>
+</body>
+</body>
+</html>
